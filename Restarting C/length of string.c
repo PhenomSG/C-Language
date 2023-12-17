87c78f85a7c05902c7ca121fee4c5337fd0eb54a
Sahaj <sahajguptarocks@gmail.com>
2023-12-17 16:51:43 +0530

    done

---
+++ Restarting C/length of string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[10] = "Sahaj";
+    int i;
+    for(i=0;s[i]!='\0';i++)
+    {
+        
+    }
+    printf("Length of string is %d",i);
+}
