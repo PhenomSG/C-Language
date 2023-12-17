7f6e8884a87fef189218c9c6291a6ddd5d49d186
Sahaj <sahajguptarocks@gmail.com>
2023-12-17 12:01:07 +0530

    comma

---
+++ Restarting C/comma_op.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = 5,4;            // it takes 5 then removes it and takes 4
+    printf("a is %d",a);
+    a = (5,4);          // it saves 5 as per operator precedence
+    printf("\na is %d",a);
+}
