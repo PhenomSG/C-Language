a225bb9d10cb4b5b1322e27838d9e96ae879f27c
Sahaj <sahajguptarocks@gmail.com>
2023-12-17 17:29:38 +0530

    done

--- Restarting C/scanf and gets functions.c
+++ Restarting C/scanf and gets functions.c
@@ -2,5 +2,15 @@
 
 int main()
 {
-    
+    // scanf function
+    char s[10];
+    printf("Enter name: ");
+    scanf("%s",s);
+    printf("%s",s);
+
+    // gets function
+    char s1[15];
+    printf("Enter: ");
+    scanf("%s",s1);
+    printf("%s",s1)
 }
