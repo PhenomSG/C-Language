ececbac659ed4e62e99de899f5e27fb2bb9bd815
Sahaj <sahajguptarocks@gmail.com>
2023-12-17 11:54:53 +0530

    1

---
+++ Restarting C/operators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  // standard input output header file
+#include <conio.h>  //console input/output
+
+int main()
+{
+    int a =10,b=20;
+    printf("Addition is %d",b+a);
+    printf("\nSubtraction is %d",b-a);
+    printf("\nMultiplication is %d",b*a);
+    printf("\nDivision is %d",b/a);
+    printf("\nModulus is %d",b%a);
+}
