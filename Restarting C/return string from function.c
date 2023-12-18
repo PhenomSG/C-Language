c04ecc4e719a2273fd3904adc5e54355c8fba0ca
Sahaj <sahajguptarocks@gmail.com>
2023-12-18 17:01:25 +0530

    string

--- Restarting C/return string from function.c
+++ Restarting C/return string from function.c
@@ -4,3 +4,16 @@
 
 # include <stdio.h>
 
+char * display();
+
+void main()
+{
+    char * str;
+    str = display();
+    printf("String is %s",str);
+}
+char * display()
+{
+    return "Jenny"; // stored in seperate memory location which you can read only
+                    // we an access even after execution of function
+}
