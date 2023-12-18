b750f04da0b37e30caa6b2c834f8737a5f91cb40
Sahaj <sahajguptarocks@gmail.com>
2023-12-18 17:19:45 +0530

    pointer to function

---
+++ Restarting C/function pointer.c
@@ -0,0 +1,24 @@
+// pointer to function
+// function pointer contains all the code
+
+// declaration
+// DataType(* pointer name)(DataType of arguement)
+
+// int * ptr(int,int) -> this means ptr is a function as it belongs to () not * due to OPERATOR PRECEDENCE
+// here, ptr is a function pointer which accepts 2 integer arguements
+
+#include <stdio.h>
+
+int sum(int,int); // fun declaration
+
+void main()
+{
+    int s = 0;
+    int (*ptr)(int,int) = &sum;
+    s = (*ptr)(2,3);
+    printf("%d",s);
+}
+int sum(int a,int b)
+{
+    return a+b;
+}
