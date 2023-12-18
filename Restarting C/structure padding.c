05f7e16e2dc2f8c9fba5b2c5496bb1a22704d4f5
Sahaj <sahajguptarocks@gmail.com>
2023-12-18 15:05:24 +0530

    struct padding

--- Restarting C/structure padding.c
+++ Restarting C/structure padding.c
@@ -1,2 +1,16 @@
 // structure padding
 // it is the process of inserting some extra bytes between variables just to align the data
+// now a days memory is WORD addressable not byte addressable
+
+// designated initialization in structures
+struct ques
+{
+    int a;
+    float b;
+    int c;
+};
+void main()
+{
+    struct ques q = {.b = 1, .c = 10, .a = 15};
+    printf("%d %f %d",q.a,q.b,q.c);
+}
