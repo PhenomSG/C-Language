f9fb0b27db1a2c5c58baaf56b81db6c3c818a754
Sahaj <sahajguptarocks@gmail.com>
2023-12-18 15:21:26 +0530

    str as arg

---
+++ Restarting C/string as arguement.c
@@ -0,0 +1,14 @@
+// string as arguement
+
+void display(char []);
+
+void main()
+{
+    char str[] = "Sahaj";
+    display(str);
+}
+
+void display(char str[])
+{
+    printf("String is %s",str);
+}
