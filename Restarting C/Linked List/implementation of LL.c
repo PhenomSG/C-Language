34e33c86c93c6088695347307923341b4df56a7a
Sahaj <sahajguptarocks@gmail.com>
2023-12-26 10:49:47 +0530

    done

--- Restarting C/Linked List/implementation of LL.c
+++ Restarting C/Linked List/implementation of LL.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+// A Single linked list node 
+struct node
+{
+    int data;           // 4bytes
+    struct node * next; // 4bytes
+};                      // total - 8 bytes
+
+int main()
+{
+    struct node * head,*newnode;
+    head = 0;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter Data: ");
+    scanf("%d",&newnode->data)
+}
