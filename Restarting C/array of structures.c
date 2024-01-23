7721da249b85738dc748a59673e413f869a0a3b4
Sahaj <sahajguptarocks@gmail.com>
2024-01-23 10:32:43 +0530

    erroe solved

--- Restarting C/array of structures.c
+++ Restarting C/array of structures.c
@@ -11,23 +11,29 @@ int main()
 {
     struct student s[3];
     int i;
-    // reading the input
-    for ( i = 0; i < 3; i++)
+
+    // Reading the input
+    for (i = 0; i < 3; i++)
     {
         printf("Enter Roll No: ");
-        scanf("%d",&s[i].rollno);
-        printf("\nEnter Name: ");
-        scanf("%s",s[i].name);
-        printf("\nEnter Marks: ");
-        scanf("%f",&s[i].marks);
+        scanf("%d", &s[i].rollno);
+
+        printf("Enter Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Enter Marks: ");
+        scanf("%f", &s[i].marks);
     }
-    // printing the input
-    for ( i = 0; i < 3; i++)
+
+    // Printing the input
+    printf("\nEntered Information:\n");
+    for (i = 0; i < 3; i++)
     {
-        printf("%d\t",s[i].rollno);
-        printf("%s\t",s[i].name);
-        printf("%f\t",s[i].marks);
+        printf("Roll No: %d\t", s[i].rollno);
+        printf("Name: %s\t", s[i].name);
+        printf("Marks: %f\t", s[i].marks);
         printf("\n");
     }
-    
+
+    return 0;
 }
