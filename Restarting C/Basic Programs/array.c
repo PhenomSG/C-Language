148ce3e7ea9ee7c52cd72ef443022e162ec5d39c
Sahaj <sahajguptarocks@gmail.com>
2024-06-05 17:22:17 +0530

    array

---
+++ Restarting C/Basic Programs/array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a[4] = {1,2,3,4};
+    // a[2] = 2[a] = *(a+2) = *(2+a)
+    printf("%d\n",a[2]);
+    printf("%d\n",*(a+2));
+    printf("%d\n",*(2+a));
+    printf("%d\n",2[a]);
+}
