2c4d26b89d5e45ee586db484a5d7d5606944c2cc
Sahaj <sahajguptarocks@gmail.com>
2024-06-05 17:33:51 +0530

    arr address

---
+++ Restarting C/Basic Programs/ques1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a[] = {1,2,3,4};
+    printf("%u\n",a);
+    printf("%d\n",*a);
+    printf("%u\n",&a);
+
+    printf("%u\n",a+1);
+    printf("%d\n",*a+1);
+    printf("%u\n",&a+1);
+}
