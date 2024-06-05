83637d9e870f7d6a26276d2ae7ff1021a9926fdb
Sahaj <sahajguptarocks@gmail.com>
2024-06-05 16:58:30 +0530

    pointers

--- Restarting C/Basic Programs/ques.c
+++ Restarting C/Basic Programs/ques.c
@@ -5,3 +5,12 @@ int main(){
     scanf("%d",pi); //  if 5
     printf("%d\n",i+5); // 10
 }
+/*
+int main(){
+    int i =10;
+    int *ptr;
+    printf("%d",*ptr);
+    // uninitialised pointer
+    // has garbage value
+}
+*/
