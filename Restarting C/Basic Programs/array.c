7fc7d7776a025b3f9fcf43e06b2668ed9dd2a952
Sahaj <sahajguptarocks@gmail.com>
2024-06-05 17:23:01 +0530

    ok

--- Restarting C/Basic Programs/array.c
+++ Restarting C/Basic Programs/array.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(){
     int a[4] = {1,2,3,4};
+    // int a[2] = {1,2,3,4}
+    // excess element ignored
     // a[2] = 2[a] = *(a+2) = *(2+a)
     printf("%d\n",a[2]);
     printf("%d\n",*(a+2));
